12f91f07ffe5483444ecf3832f5c8fe188c9d227
Jaime Dias <jaimejosediasnt@gmail.com>
2023-07-12 04:58:06 -0300

    fix circular dependency

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
     // Transpile Swagger UI React. https://github.com/swagger-api/swagger-ui/issues/8245
     transpilePackages: ['react-syntax-highlighter', 'swagger-client', 'swagger-ui-react'],
-
+    experimental: {
+        serverComponentsExternalPackages: ["typeorm"],
+      },
 }
 
 module.exports = nextConfig
